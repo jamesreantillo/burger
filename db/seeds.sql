edfebc887808710e97987b645218aa7d5591e68a
James Ryann Reantillo <james@Jamess-MacBook-Air.local>
2017-10-31 23:46:15 -0700

    final

---
+++ db/seeds.sql
@@ -0,0 +1,7 @@
+
+INSERT INTO `burgers` (burger_name, devoured, date) 
+VALUES ('Amazing Aloha', false, CURRENT_TIMESTAMP),
+
+       ('Double Longganisa Burger', false, CURRENT_TIMESTAMP),
+
+       ('Double Bartburger', false, CURRENT_TIMESTAMP);
